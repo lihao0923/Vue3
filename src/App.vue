9de1b56ce039dbda5e09850c24d9122df8788616
lihao923 <lihao923@163.com>
2022-04-23 19:12:56 +0800

    modified files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
+// This starter template is using Vue 3 <script setup> SFCs blabla
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
 </script>
